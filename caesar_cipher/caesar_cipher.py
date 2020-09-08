7376f1747b8323d78ca9754915dc6d1a0cc9e961
3madov-77 <emadzxy7@gmail.com>
2020-09-08 21:16:22 +0300

    half the way

---
+++ caesar_cipher/caesar_cipher.py
@@ -0,0 +1,40 @@
+# import nltk
+# nltk.download('words')
+
+from nltk.corpus import words
+word_list_orginal = words.words()
+import random
+
+word_list = set([word.lower() for word in word_list_orginal])
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(words, key):
+    encrypted_words = ''
+
+    for char in words.lower():
+        if char in alphabet:
+            encrypted_letter = alphabet[(alphabet.index(char.lower()) + key) % len(alphabet)]
+            encrypted_words += encrypted_letter
+        else:
+            encrypted_words += char
+    return encrypted_words    
+
+
+def decrypt(words):
+
+    def english_words(list_of_words):
+        number_correct = 0
+        for word in list_of_words:
+            if word in word_list:
+                number_correct += 1
+        if number_correct/len(list_of_words) >= 0.5:
+            return True
+        return False
+
+    for key in range(len(alphabet)):    
+        a = encrypt(words, (-1*(key)))
+        b = english_words(a.split(' '))
+        if b:
+            return a
+
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,11 @@
 from caesar_cipher import __version__
+from caesar_cipher.caesar_cipher import *
 
+
+string = "“I hope that in this year to come, you make mistakes. Because if you are making mistakes, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You’re doing things you’ve never done before, and more importantly, you’re doing something.”"
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_encrypt():
+    assert encrypted_string == encrypt(string, 5) == "“n mtuj ymfy ns ymnx djfw yt htrj, dtz rfpj rnxyfpjx. gjhfzxj nk dtz fwj rfpnsl rnxyfpjx, ymjs dtz fwj rfpnsl sjb ymnslx, ywdnsl sjb ymnslx, qjfwsnsl, qnansl, uzxmnsl dtzwxjqk, hmfslnsl dtzwxjqk, hmfslnsl dtzw btwqi. dtz’wj itnsl ymnslx dtz’aj sjajw itsj gjktwj, fsi rtwj nrutwyfsyqd, dtz’wj itnsl xtrjymnsl.”"
